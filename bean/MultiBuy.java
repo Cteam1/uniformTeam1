01013ca45bf9aedf9e3aaaddf2f288c0772c3bc1
Cteam1 <137234859+Cteam1@users.noreply.github.com>
2023-06-22 16:19:44 +0900

    Add files via upload

---
+++ bean/MultiBuy.java
@@ -0,0 +1,21 @@
+package bean;
+
+public class MultiBuy {
+
+	//購入数
+	private int quantity;
+
+	public MultiBuy() {
+		//コンストラクタ
+		this.quantity= 0;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
