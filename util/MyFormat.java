9b5f3220ef5fb16a215cadd3c35ee7ca9448111a
Cteam1 <137234859+Cteam1@users.noreply.github.com>
2023-06-26 17:11:58 +0900

    Add files via upload

---
+++ util/MyFormat.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.text.DecimalFormat;
+
+
+public class MyFormat {
+
+
+
+	//インスタンス化と同時にpriceのフォーマット("最初につく文字,0の数でカンマ区切り,0の数だけ小数点を表示")
+	public String moneyFormat (int price) {
+
+		DecimalFormat df = new DecimalFormat("\\,000");
+
+		df.format(price);
+
+		return df.format(price);
+
+	}
+
+
+}
