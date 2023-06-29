d057164b5cd9cbda283a5e35f905e52c8061587a
Cteam1 <137234859+Cteam1@users.noreply.github.com>
2023-06-29 13:17:25 +0900

    Add files via upload

--- servlet/PaymentServlet.java
+++ servlet/PaymentServlet.java
@@ -1,16 +1,11 @@
 package servlet;
 
-import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import bean.Admin;
-import bean.Order;
-import dao.OrderDAO;
+import bean.*;
+import dao.*;
 import util.SendMail;
 
 public class PaymentServlet extends HttpServlet {
@@ -19,84 +14,76 @@ public class PaymentServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	String error = "";
-	String cmd = "";
-	try {
+		String error = "";
+		String cmd = "";
+		try {
 
-		HttpSession session = request.getSession();
+			HttpSession session = request.getSession();
 
-		Admin admin = (Admin) session.getAttribute("admin");
+			Admin admin = (Admin) session.getAttribute("admin");
 
-		if (admin == null) { // セッション切れの場合
-			error = "セッションが切れたため、注文詳細を確認することができませんでした。";
+			if (admin == null) { // セッション切れの場合
+				error = "セッションが切れたため、注文詳細を確認することができませんでした。";
+				cmd = "logout";
+				return;
+			}
+
+			// 画面からの入力情報を受け取るためのエンコードを設定します。
+			request.setCharacterEncoding("UTF-8");
+
+			// 入金ステータス
+			String payment = request.getParameter("payment");
+
+			// 発送ステータス
+			String send = request.getParameter("send");
+
+			// 引き数用変数name
+			String name = request.getParameter("name");
+
+			// 引き数用変数ordertime
+			String orderTime = request.getParameter("orderTime");
+
+			// データベースを更新します。
+			// ステータスが入金済み-未発送の時
+			// メール発送あり
+			OrderDAO orderDao = new OrderDAO();
+			if (payment.equals("入金済み") && send.equals("未発送")) {
+				orderDao.paymentUpdate(payment, name, orderTime);
+
+				// mail
+				send = null;
+				SendMail sendMail = new SendMail();
+				sendMail.send(name, payment, send);
+
+				// ステータスが入金済み-発送済の時
+				// メール発送あり
+			} else if (payment.equals("入金済み") && send.equals("発送済み")) {
+				// orderDao.sendUpdate(send, name, orderTime);
+				orderDao.StatusReset(send, payment, name, orderTime);
+
+				// mail
+				payment = null;
+				SendMail sendMail = new SendMail();
+				sendMail.send(name, payment, send);
+
+				// 未入金-未発送の状態に戻すとき
+			} else if (payment.equals("入金済み") && send.equals("発送準備中")) {
+				orderDao.StatusReset(send, payment, name, orderTime);
+			}
+
+		} catch (Exception e) {
+			// ★エラーをキャッチした場合、Error.jspへフォワード処理を行う
+			error = "DB接続エラーの為、支払い状況、発送状況の変更は出来ませんでした。";
 			cmd = "logout";
-			return;
+		} finally {
+			if (error.equals("")) {
+				request.getRequestDispatcher("/orderList").forward(request, response);
+			} else {
+				request.setAttribute("cmd", cmd);
+				request.setAttribute("error", error);
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+			}
+
 		}
-
-		//画面からの入力情報を受け取るためのエンコードを設定します。
-		request.setCharacterEncoding("UTF-8");
-
-		//入金ステータス
-		String payment = request.getParameter("payment");
-
-		//発送ステータス
-		String send = request.getParameter("send");
-
-		//引き数用変数name
-		String name = request.getParameter("name");
-
-		//引き数用変数ordertime
-		String orderTime = request.getParameter("orderTime");
-
-
-
-		//データベースを更新します。
-		//ステータスが入金済み-未発送の時
-		//メール発送あり
-		OrderDAO orderDao = new OrderDAO();
-		if(payment.equals("入金済み") && send.equals("未発送")){
-			orderDao.paymentUpdate(payment, name, orderTime);
-
-		//mail
-			send = null;
-			SendMail sendMail = new SendMail();
-			sendMail.send(name, payment, send);
-
-		//ステータスが入金済み-発送済の時
-		//メール発送あり
-		}else if(payment.equals("入金済み") && send.equals("発送済み")) {
-			//orderDao.sendUpdate(send, name, orderTime);
-			orderDao.StatusReset(send,payment, name, orderTime);
-
-			//mail
-			payment = null;
-			SendMail sendMail = new SendMail();
-			sendMail.send(name, payment, send);
-
-
-		//未入金-未発送の状態に戻すとき
-		}else if(payment.equals("入金済み") && send.equals("発送準備中")) {
-			orderDao.StatusReset(send, payment, name, orderTime);
-		}
-
-	} catch (Exception e) {
-		//★エラーをキャッチした場合、Error.jspへフォワード処理を行う
-		error = "DB接続エラーの為、支払い状況、発送状況の変更は出来ませんでした。";
-		cmd = "logout";
-	} finally {
-		if(error.equals("")){
-			request.getRequestDispatcher("/orderList").forward(request, response);
-		} else {
-			request.setAttribute("cmd", cmd);
-			request.setAttribute("error", error);
-			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
-	}
-
-
-
-
-
-
-}
 	}
 }
