6515ad9e030fdf20f5c56ad455afdad26193c521
Cteam1 <137234859+Cteam1@users.noreply.github.com>
2023-06-29 13:12:32 +0900

    Add files via upload

--- servlet/LogoutServlet.java
+++ servlet/LogoutServlet.java
@@ -4,22 +4,20 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-
 public class LogoutServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		//セッション情報をクリアする
+		// セッション情報をクリアする
 		HttpSession session = request.getSession();
 
 		if (session != null) {
 			session.invalidate();
 		}
 
-		//login.jspにフォワードする
-		request.getRequestDispatcher("/view/login.jsp").forward(request,response);
+		// login.jspにフォワードする
+		request.getRequestDispatcher("/view/login.jsp").forward(request, response);
 	}
 }
